57dd6a12247c50bcd12ec2bda65265b1d7ebb04c
Harsh kumar <mister.harshkumar@gmail.com>
2024-10-16 21:25:18 -0700

    Update BASE_URL for local development

--- frontend/src/lib/db.ts
+++ frontend/src/lib/db.ts
@@ -1,5 +1,5 @@
 
-// const BASE_URL = "http://195.26.242.78:3003";
-const BASE_URL="http://127.0.0.1:3003";
+const BASE_URL = "http://195.26.242.78:3003";
+// const BASE_URL="http://127.0.0.1:3003";
 // const BASE_URL="https://reportcard-v1ca.onrender.com";
 export default BASE_URL;
